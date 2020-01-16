f8fde578661fae272af8048ba865f4d6e6ff40b1
niteshthapa <nitesh_thapa@ymail.com>
2020-01-16 08:16:50 +0530

    server for production

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const app = express();
 //----------------------
 const connectDB = require('./config/db');
 connectDB();
+const path = require('path');
 
 //Middelware
 //----------------------
@@ -19,6 +20,15 @@ app.get('/',(req,res)=>{
 app.use('/api/users',require('./routes/users'));
 app.use('/api/auth',require('./routes/auth'));
 app.use('/api/dashboard',require('./routes/dashboard'));
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+    // Set static folder
+    app.use(express.static('client/build'));
+  
+    app.get('*', (req, res) =>
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    );
+  }
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, ()=> console.log(`Server is running on ${PORT}`))
